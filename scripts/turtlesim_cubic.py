c9b053ed94a3742acab43334e816214f23f72044
ABD-01 <myac931@gmail.com>
2021-11-06 18:58:18 +0530

    Created Hermite curve based traj

---
+++ scripts/turtlesim_cubic.py
@@ -0,0 +1,168 @@
+#!/usr/bin/env python3
+
+import math
+import time
+
+from numpy.linalg.linalg import norm
+import rospy
+# import numpy as np
+from numpy import array, sin, cos, dot, arctan2, linalg, ones_like, linspace, arange, vstack
+from turtlesim.msg import Pose
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Float32MultiArray
+import argparse
+from namedlist import namedlist
+from copy import deepcopy, error
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+
+K = namedlist("K", ["kp", "ki", "kd"])
+
+class Hermite:
+
+    H = array([[2,-2,1,1],[-3,3,-2,-1],[0,0,1,0],[1,0,0,0]])
+
+    def __init__(self, P0, P1, P_dot0, P_dot1) -> None:
+        P = array([P0, P1, P_dot0, P_dot1])
+        assert P.shape[0] == 4
+        self.A = dot(self.H, P)
+        
+    def __call__(self, time):
+        Time = vstack([time**3, time**2, time, ones_like(time)])
+        return dot(Time.T, self.A)
+
+    def view_traj(self):
+        # t = arange(0, 1, 0.05)
+        # traj = self.__call__(t)
+        # plt.plot(traj[:,0], traj[:,1])
+        # plt.show()
+        # print(traj)
+
+        fig, ax = plt.subplots()
+        xdata, ydata = [], []
+        ln, = plt.plot([], [], 'ro-')
+
+        def init():
+            ax.set_xlim(0, 11)
+            ax.set_ylim(0, 11)
+            return ln,
+
+        def update(frame):
+            traj = self(frame).squeeze()
+            xdata.append(traj[0])
+            ydata.append(traj[1])
+            ln.set_data(xdata, ydata)
+            return ln,
+
+        ani = FuncAnimation(fig, update, frames=linspace(0, 1, 20),
+                            init_func=init, blit=True)
+        plt.show()
+
+        
+
+class PID:
+    def __init__(
+        self,
+        final_pose: Pose,
+        k=K(2, 0.0, 0),
+        ka=K(2, 0.0, 0),
+        loop_rate=10,
+
+    ):
+
+        self.current_pose = None
+        self.error = 0
+        self.final_pose = final_pose
+
+        rospy.init_node("turtlesim_pid", anonymous=True)
+        self.loop_rate = rospy.Rate(loop_rate)
+        self.subscriber = rospy.Subscriber(
+            "/turtle1/pose", Pose, callback=self.callback
+        )
+        time.sleep(4)
+        self.publisher = rospy.Publisher("/turtle1/cmd_vel", Twist, queue_size=10)
+        self.tracker = rospy.Publisher("/plot/error", Float32MultiArray, queue_size=10)
+        self.k = k
+        self.ka = ka
+
+        self.hermite = Hermite(
+            [self.current_pose.x, self.current_pose.y], # start (x,y)
+            [self.final_pose.x, self.final_pose.y],     # final (x,y)
+            [cos(self.current_pose.theta), sin(self.current_pose.theta)], # start (x', y')
+            [cos(self.final_pose.theta), sin(self.final_pose.theta)]      # final (x', y')
+        )
+        self.hermite.view_traj()
+
+    def callback(self, pose_msg):
+        self.current_pose = pose_msg
+        # print("Pose cb(x: {:.2f}  y: {:.2f}  theta: {:.2f})".format(
+        #         self.current_pose.x, self.current_pose.y, self.current_pose.theta))
+
+    def get_error(self, p_desired):
+        diff = p_desired - array([self.current_pose.x, self.current_pose.y])
+        distance = linalg.norm(diff)
+        angle = arctan2(diff[0], diff[1])
+        return float(distance), float(angle)
+
+    def move(self):
+        vel_msg = Twist()
+        error_old = 0
+        E = 0
+
+        # t0 = rospy.Time.now().to_sec()
+        time = 0
+        i = 0
+        while True:
+            # t1 =  rospy.Time.now().to_sec()
+            # time = t1 - t0
+            if i%2 == 0:
+                time += 0.05
+
+            p_desired = self.hermite(time)
+            print("Desired point:", p_desired)
+            error, error_a = self.get_error(p_desired.squeeze())
+            print("Error:", error)
+            linear_speed = error * self.k.kp
+            angular_speed  = error_a * self.ka.kp
+
+            print(linear_speed, angular_speed)
+            vel_msg.linear.x = linear_speed
+            vel_msg.angular.z = angular_speed
+
+            if time > 1:
+                vel_msg.linear.x = 0
+                vel_msg.angular.z = 0
+
+            self.publisher.publish(vel_msg)
+            self.loop_rate.sleep()
+
+
+
+if __name__ == "__main__":
+    try:
+        parser = argparse.ArgumentParser(description="Input final pose")
+        parser.add_argument(
+            "-x", "--x", type=float, required=True, help="Final x position"
+        )
+        parser.add_argument(
+            "-y", "--y", type=float, required=True, help="Final y position"
+        )
+        parser.add_argument(
+            "-t", "--theta", type=float, required=True, help="Final angle (in degrees)"
+        )
+
+        args = parser.parse_args()
+
+        final_pose = Pose()
+        final_pose.x = args.x
+        final_pose.y = args.y
+        final_pose.theta = math.radians(args.theta)
+
+        pid = PID(final_pose)
+        # time.sleep(2)
+        pid.move()
+        rospy.spin()
+
+    except rospy.ROSInterruptException:
+        rospy.loginfo("node terminated.")
